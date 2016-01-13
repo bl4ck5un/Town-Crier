e84bc1d5c7de241851719f0217380a27928d09bf
Fan Zhang <bl4ck5unxx@gmail.com>
2016-01-13 13:48:40 -0500

    Read mbedTLS code.

---
+++ code/TC/README.txt
@@ -0,0 +1,7 @@
+Purpose of SampleEnclave
+
+The project demonstrates several fundamental usages:
+    1. Initializing and destroying an enclave
+    2. Creating ECALLs or OCALLs
+    3. Calling trusted libraries inside the enclave
+
---
+++ code/TC/TLSlib/TLSlib_t.h
@@ -0,0 +1,23 @@
+#ifndef TLSLIB_T_H__
+#define TLSLIB_T_H__
+
+#include <stdint.h>
+#include <wchar.h>
+#include <stddef.h>
+#include "sgx_edger8r.h" /* for sgx_ocall etc. */
+
+
+#define SGX_CAST(type, item) ((type)(item))
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif
