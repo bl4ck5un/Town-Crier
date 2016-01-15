82a2efd77f5d2a7558228c592c6479c74fdca7d4
Fan Zhang <bl4ck5unxx@gmail.com>
2016-01-15 11:58:09 -0500

    Add code for trusted time source.

---
+++ code/SGX_Playground/TrustedTime/App/Enclave_u.c
@@ -0,0 +1,172 @@
+#include "Enclave_u.h"
+
+
+
+typedef struct ms_ocall_print_string_t {
+	char* ms_str;
+} ms_ocall_print_string_t;
+
+typedef struct ms_create_session_ocall_t {
+	sgx_status_t ms_retval;
+	uint32_t* ms_sid;
+	uint8_t* ms_dh_msg1;
+	uint32_t ms_dh_msg1_size;
+	uint32_t ms_timeout;
+} ms_create_session_ocall_t;
+
+typedef struct ms_exchange_report_ocall_t {
+	sgx_status_t ms_retval;
+	uint32_t ms_sid;
+	uint8_t* ms_dh_msg2;
+	uint32_t ms_dh_msg2_size;
+	uint8_t* ms_dh_msg3;
+	uint32_t ms_dh_msg3_size;
+	uint32_t ms_timeout;
+} ms_exchange_report_ocall_t;
+
+typedef struct ms_close_session_ocall_t {
+	sgx_status_t ms_retval;
+	uint32_t ms_sid;
+	uint32_t ms_timeout;
+} ms_close_session_ocall_t;
+
+typedef struct ms_invoke_service_ocall_t {
+	sgx_status_t ms_retval;
+	uint8_t* ms_pse_message_req;
+	uint32_t ms_pse_message_req_size;
+	uint8_t* ms_pse_message_resp;
+	uint32_t ms_pse_message_resp_size;
+	uint32_t ms_timeout;
+} ms_invoke_service_ocall_t;
+
+typedef struct ms_sgx_oc_cpuidex_t {
+	int* ms_cpuinfo;
+	int ms_leaf;
+	int ms_subleaf;
+} ms_sgx_oc_cpuidex_t;
+
+typedef struct ms_sgx_thread_wait_untrusted_event_ocall_t {
+	int ms_retval;
+	void* ms_self;
+} ms_sgx_thread_wait_untrusted_event_ocall_t;
+
+typedef struct ms_sgx_thread_set_untrusted_event_ocall_t {
+	int ms_retval;
+	void* ms_waiter;
+} ms_sgx_thread_set_untrusted_event_ocall_t;
+
+typedef struct ms_sgx_thread_setwait_untrusted_events_ocall_t {
+	int ms_retval;
+	void* ms_waiter;
+	void* ms_self;
+} ms_sgx_thread_setwait_untrusted_events_ocall_t;
+
+typedef struct ms_sgx_thread_set_multiple_untrusted_events_ocall_t {
+	int ms_retval;
+	void** ms_waiters;
+	size_t ms_total;
+} ms_sgx_thread_set_multiple_untrusted_events_ocall_t;
+
+static sgx_status_t SGX_CDECL Enclave_ocall_print_string(void* pms)
+{
+	ms_ocall_print_string_t* ms = SGX_CAST(ms_ocall_print_string_t*, pms);
+	ocall_print_string((const char*)ms->ms_str);
+	return SGX_SUCCESS;
+}
+
+static sgx_status_t SGX_CDECL Enclave_create_session_ocall(void* pms)
+{
+	ms_create_session_ocall_t* ms = SGX_CAST(ms_create_session_ocall_t*, pms);
+	ms->ms_retval = create_session_ocall(ms->ms_sid, ms->ms_dh_msg1, ms->ms_dh_msg1_size, ms->ms_timeout);
+	return SGX_SUCCESS;
+}
+
+static sgx_status_t SGX_CDECL Enclave_exchange_report_ocall(void* pms)
+{
+	ms_exchange_report_ocall_t* ms = SGX_CAST(ms_exchange_report_ocall_t*, pms);
+	ms->ms_retval = exchange_report_ocall(ms->ms_sid, ms->ms_dh_msg2, ms->ms_dh_msg2_size, ms->ms_dh_msg3, ms->ms_dh_msg3_size, ms->ms_timeout);
+	return SGX_SUCCESS;
+}
+
+static sgx_status_t SGX_CDECL Enclave_close_session_ocall(void* pms)
+{
+	ms_close_session_ocall_t* ms = SGX_CAST(ms_close_session_ocall_t*, pms);
+	ms->ms_retval = close_session_ocall(ms->ms_sid, ms->ms_timeout);
+	return SGX_SUCCESS;
+}
+
+static sgx_status_t SGX_CDECL Enclave_invoke_service_ocall(void* pms)
+{
+	ms_invoke_service_ocall_t* ms = SGX_CAST(ms_invoke_service_ocall_t*, pms);
+	ms->ms_retval = invoke_service_ocall(ms->ms_pse_message_req, ms->ms_pse_message_req_size, ms->ms_pse_message_resp, ms->ms_pse_message_resp_size, ms->ms_timeout);
+	return SGX_SUCCESS;
+}
+
+static sgx_status_t SGX_CDECL Enclave_sgx_oc_cpuidex(void* pms)
+{
+	ms_sgx_oc_cpuidex_t* ms = SGX_CAST(ms_sgx_oc_cpuidex_t*, pms);
+	sgx_oc_cpuidex(ms->ms_cpuinfo, ms->ms_leaf, ms->ms_subleaf);
+	return SGX_SUCCESS;
+}
+
+static sgx_status_t SGX_CDECL Enclave_sgx_thread_wait_untrusted_event_ocall(void* pms)
+{
+	ms_sgx_thread_wait_untrusted_event_ocall_t* ms = SGX_CAST(ms_sgx_thread_wait_untrusted_event_ocall_t*, pms);
+	ms->ms_retval = sgx_thread_wait_untrusted_event_ocall((const void*)ms->ms_self);
+	return SGX_SUCCESS;
+}
+
+static sgx_status_t SGX_CDECL Enclave_sgx_thread_set_untrusted_event_ocall(void* pms)
+{
+	ms_sgx_thread_set_untrusted_event_ocall_t* ms = SGX_CAST(ms_sgx_thread_set_untrusted_event_ocall_t*, pms);
+	ms->ms_retval = sgx_thread_set_untrusted_event_ocall((const void*)ms->ms_waiter);
+	return SGX_SUCCESS;
+}
+
+static sgx_status_t SGX_CDECL Enclave_sgx_thread_setwait_untrusted_events_ocall(void* pms)
+{
+	ms_sgx_thread_setwait_untrusted_events_ocall_t* ms = SGX_CAST(ms_sgx_thread_setwait_untrusted_events_ocall_t*, pms);
+	ms->ms_retval = sgx_thread_setwait_untrusted_events_ocall((const void*)ms->ms_waiter, (const void*)ms->ms_self);
+	return SGX_SUCCESS;
+}
+
+static sgx_status_t SGX_CDECL Enclave_sgx_thread_set_multiple_untrusted_events_ocall(void* pms)
+{
+	ms_sgx_thread_set_multiple_untrusted_events_ocall_t* ms = SGX_CAST(ms_sgx_thread_set_multiple_untrusted_events_ocall_t*, pms);
+	ms->ms_retval = sgx_thread_set_multiple_untrusted_events_ocall((const void**)ms->ms_waiters, ms->ms_total);
+	return SGX_SUCCESS;
+}
+
+static const struct {
+	size_t nr_ocall;
+	void * func_addr[10];
+} ocall_table_Enclave = {
+	10,
+	{
+		(void*)(uintptr_t)Enclave_ocall_print_string,
+		(void*)(uintptr_t)Enclave_create_session_ocall,
+		(void*)(uintptr_t)Enclave_exchange_report_ocall,
+		(void*)(uintptr_t)Enclave_close_session_ocall,
+		(void*)(uintptr_t)Enclave_invoke_service_ocall,
+		(void*)(uintptr_t)Enclave_sgx_oc_cpuidex,
+		(void*)(uintptr_t)Enclave_sgx_thread_wait_untrusted_event_ocall,
+		(void*)(uintptr_t)Enclave_sgx_thread_set_untrusted_event_ocall,
+		(void*)(uintptr_t)Enclave_sgx_thread_setwait_untrusted_events_ocall,
+		(void*)(uintptr_t)Enclave_sgx_thread_set_multiple_untrusted_events_ocall,
+	}
+};
+
+sgx_status_t showtime(sgx_enclave_id_t eid)
+{
+	sgx_status_t status;
+	status = sgx_ecall(eid, 0, &ocall_table_Enclave, NULL);
+	return status;
+}
+
+sgx_status_t create_session(sgx_enclave_id_t eid)
+{
+	sgx_status_t status;
+	status = sgx_ecall(eid, 1, &ocall_table_Enclave, NULL);
+	return status;
+}
+
---
+++ code/SGX_Playground/TrustedTime/Enclave/Enclave_t.h
@@ -0,0 +1,35 @@
+#ifndef ENCLAVE_T_H__
+#define ENCLAVE_T_H__
+
+#include <stdint.h>
+#include <wchar.h>
+#include <stddef.h>
+#include "sgx_edger8r.h" /* for sgx_ocall etc. */
+
+
+#define SGX_CAST(type, item) ((type)(item))
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void showtime();
+void create_session();
+
+sgx_status_t SGX_CDECL ocall_print_string(const char* str);
+sgx_status_t SGX_CDECL create_session_ocall(sgx_status_t* retval, uint32_t* sid, uint8_t* dh_msg1, uint32_t dh_msg1_size, uint32_t timeout);
+sgx_status_t SGX_CDECL exchange_report_ocall(sgx_status_t* retval, uint32_t sid, uint8_t* dh_msg2, uint32_t dh_msg2_size, uint8_t* dh_msg3, uint32_t dh_msg3_size, uint32_t timeout);
+sgx_status_t SGX_CDECL close_session_ocall(sgx_status_t* retval, uint32_t sid, uint32_t timeout);
+sgx_status_t SGX_CDECL invoke_service_ocall(sgx_status_t* retval, uint8_t* pse_message_req, uint32_t pse_message_req_size, uint8_t* pse_message_resp, uint32_t pse_message_resp_size, uint32_t timeout);
+sgx_status_t SGX_CDECL sgx_oc_cpuidex(int cpuinfo[4], int leaf, int subleaf);
+sgx_status_t SGX_CDECL sgx_thread_wait_untrusted_event_ocall(int* retval, const void* self);
+sgx_status_t SGX_CDECL sgx_thread_set_untrusted_event_ocall(int* retval, const void* waiter);
+sgx_status_t SGX_CDECL sgx_thread_setwait_untrusted_events_ocall(int* retval, const void* waiter, const void* self);
+sgx_status_t SGX_CDECL sgx_thread_set_multiple_untrusted_events_ocall(int* retval, const void** waiters, size_t total);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif
