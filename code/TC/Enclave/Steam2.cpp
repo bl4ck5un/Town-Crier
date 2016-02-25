f612749eb1ec1227432af60ed3b4f784a5c94032
cromak4 <cromak4@gmail.com>
2016-02-25 02:22:05 -0500

    updated example

--- code/TC/Enclave/Steam2.cpp
+++ code/TC/Enclave/Steam2.cpp
@@ -266,19 +266,9 @@ int main(int argc, char* argv[]) {
     printf("Starting...\n");
     // needs as input time of request, T_B time for response, key, account # ID_B, list of items L_B, account # ID_S 
     // I'm not sure how we get time... but so long as we get it somehow...
-    rc = get_steam_transaction(listB, 1, "32884794", 1355220300, "7978F8EDEF9695B57E72EC468E5781AD", &ret);
-
-    if (rc == 0 && ret == 1) {
-        printf("Found a trade, %d, %d\n", rc, ret);
-    }
+    rc = get_steam_transaction(listB, 1, "32884794", 1456380265, "7978F8EDEF9695B57E72EC468E5781AD", &ret);
     printf("%d, %d\n", rc, ret);
-
-    rc = get_steam_transaction(test2, 2, "32884794", 1355220300, "7978F8EDEF9695B57E72EC468E5781AD", &ret);
-    printf("%d, %d\n", rc, ret);
-
-    // should fail
-    rc = get_steam_transaction(test2, 1, "32884794", 1355220300, "7978F8EDEF9695B57E72EC468E5781AD", &ret);
-    printf("%d, %d\n", rc, ret);
+    //rc should be 0, ret should be 1
 
     return 0;
 }
