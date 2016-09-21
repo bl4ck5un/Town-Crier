56eec4fd97fab522b3eee5b7b829bbea5b0feca5
Fan Zhang <bl4ck5unxx@gmail.com>
2016-09-21 16:31:36 -0400

    Add CA certs for www.ups.com (COMODO_RSA_CA.pem)

--- code/TC/Enclave/UPS_Tracking.cpp
+++ code/TC/Enclave/UPS_Tracking.cpp
@@ -9,7 +9,7 @@ int ups_tracking (char* tracking_num)
 	LL_LOG("Tracking %s", tracking_num);
 
 	char buf[3*1024];
-	int ret = get_page_on_ssl("ups.com", "/", NULL, 0, (unsigned char*)buf, 3*1024); 
+	int ret = get_page_on_ssl("www.ups.com", "/", NULL, 0, (unsigned char*)buf, 3*1024); 
 	LL_LOG("Done.");
 
 	return 0;
