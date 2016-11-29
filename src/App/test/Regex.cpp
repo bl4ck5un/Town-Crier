aaf45f826523e943498f37066fab19c94090983d
Oscar Zegarra <oaz2@cornell.edu>
2016-11-29 14:40:52 -0500

    Adding support for regex

---
+++ src/App/test/Regex.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+#include "../Init.h"
+#include "../Enclave_u.h"
+
+#include "../EthRPC.h"
+#include "Converter.h"
+#include <vector>
+
+TEST(RegEx, regex){
+    sgx_enclave_id_t eid;
+    int ret = initialize_enclave(ENCLAVE_FILENAME, &eid);
+    ASSERT_EQ(SGX_SUCCESS, ret);
+
+   //Note: Test is on outdated flights
+   // flight_self_test(eid, &ret);
+   // ASSERT_EQ(0, ret);
+    regex_self_test(eid, &ret);
+    ASSERT_EQ(0, ret);
+
+}
