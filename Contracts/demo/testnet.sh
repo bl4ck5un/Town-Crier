b989a0b502d1ee0e245dca8863348d1bb4975498
iseriohn <jyamy95@126.com>
2016-12-13 14:15:34 -0500

    for testnet

--- Contracts/demo/testnet.sh
+++ Contracts/demo/testnet.sh
@@ -1,4 +1,10 @@
 geth --testnet \
-     --ipcpath=~/.ethereum/geth.ipc \
-     --rpcapi="db,eth,ne,web3,personal,crypto-js" \
-     console
+     --ipcpath ~/.ethereum/geth.ipc \
+     --datadir ~/.ethereum \
+     --rpcapi "db,eth,ne,web3,personal,crypto-js" \
+     --port 30000 \
+     --rpc \
+     --rpcaddr 127.0.0.1\
+     --rpcport 8200\
+     --rpccorsdomain '*' \
+    console
